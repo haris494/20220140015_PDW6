731af72d2083a79e4fff975930340d467008ef6a
haris49 <hariis.459@gmail.com>
2024-04-09 09:19:55 +0700

    first commit

---
+++ js.CH5.js
@@ -0,0 +1,14 @@
+// Menambahkan event listener untuk elemen video
+video.addEventListener("click", function() {
+    alert("Ini video Pemandangan");
+});
+
+// Menambahkan event listener untuk elemen audio
+audio.addEventListener("click", function() {
+    alert("Ini audio pemandangan");
+});
+
+// Menambahkan event listener untuk elemen gambar
+gambar.addEventListener("click", function() {
+    alert("Ini gambar Pemandangan");
+});
